6a8fa7b7a15a500753c95c7ba82020c196356662
klb <plbartnicki@gmail.com>
2018-08-19 13:52:52 +0200

    komentarz

--- kat7tdd/src/test/java/ArrayHelperTest.java
+++ kat7tdd/src/test/java/ArrayHelperTest.java
@@ -1,31 +1,49 @@
 import com.klb.commons.ArrayHelper;
+import com.klb.commons.RangeArrayException;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class ArrayHelperTest {
 
     //testuje czy metoda replace dziala poprawnie gdy dostaje poprawne argumenty
     @Test
-    public void testReplaceValidArguments() {
+    public void testReplaceValidArguments() throws RangeArrayException {
         String []tab={"xyz", "aaaa", "wwwww", "test", "niesporczak"};
 
         ArrayHelper.replace(tab, "piwo", 1, 3);
 
         //sprawdzamy czy mtoda replace zadzialala dobrze
         for(int k = 1; k <= 3; k++) {
-            if( DOKONCZ ) {
+            if( !tab[k].equals("piwo") ) {
                 fail();   //niezalicza testu
             }
         }
 
-        DOKONCZ????
     }
 
     //testuje czy replace rzuca wyjatek dla niepoprawnych argumentow
+    //zaliczy test jesli metoda rzuci wyjatek, w.p.w. nie powinna zaliczyc testu
     @Test
     public void testReplaceInvalidArguments() {
-        //METODA KTORA JEST PUSTA ZALICZA TET
+        //METODA KTORA JEST PUSTA ZALICZA TEst
+//        try {
+//            ArrayHelper.replace(new String[]{}, "piwo", -1, 3);
+//            fail();
+//        } catch (RangeArrayException ex) {
+//
+//        }
+
+        boolean q = false;   //zakladam ze metoda nie rzuci wyjatku
+        try {
+            ArrayHelper.replace(new String[]{}, "piwo", -1, 3);
+        } catch (RangeArrayException ex) {
+            q = true;  //informacja ze metoda rzucila wyjatek
+        }
+
+        assertTrue(q);
+
     }
 
  }
